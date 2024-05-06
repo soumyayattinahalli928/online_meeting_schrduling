857eb07a3ac3cc028a4214e1d672e0b47ab781dc
aishwarya0608 <aishwaryasurendra98@gmail.com>
2024-05-06 15:58:02 +0530

    frontend pages updated

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
 import './index.css';
-import AdminLogin from './components/AdminLogin';
+//import AdminLogin from './components/AdminLogin';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import UserLogin from './Pages/UserLogin';
-import AdminReg from './components/AdminReg';
-import UserReg from './Pages/UserReg';
+//import UserLogin from './Pages/UserLogin';
+//import AdminReg from './components/AdminReg';
+//import UserReg from './Pages/UserReg';
+//import AddAnUser from'./components/AddAnUser';
+import ForgotPassword from'./components/ForgotPassword';
+
+
 function App() {
   return (
-    <div className="App">
-     {/* <AdminLogin/>
- <UserLogin/>  */}
-
+   <div className="App">
+    {/* <AdminLogin/>
+ <UserLogin/>  
       <AdminReg/>
-
-      {/* <UserReg/> */}
-    </div>
+  <UserReg/> 
+  <AddAnUser/>*/}
+  <ForgotPassword/>
+  </div>
   );
 }
-
 export default App;
+
+
+
+
